fe007299f8c9ad041fe2902bafaa040d545b45c5
ducklee89 <awesomelee89@gmail.com>
2019-12-22 23:36:41 +0900

    nice commit

---
+++ coocoodot.py
@@ -0,0 +1,12 @@
+import module
+from module import person1
+
+module.greeting("hoohoo")
+
+
+a = module.person1["country"]
+
+print(a)
+
+print(person1["name"])
+
---
+++ nicefoo.py
@@ -0,0 +1,12 @@
+import module as mx
+import platform
+
+a = mx.person1["name"]
+print(a)
+
+x = platform.system()
+print(x)
+
+x = dir(platform)
+print(x)
+
