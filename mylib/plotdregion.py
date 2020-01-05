54234bf8cc1ff20b5a50616ba1ddf2880bc2ce7a
ducklee89 <awesomelee89@gmail.com>
2020-01-06 01:13:11 +0900

    ML basic

---
+++ mylib/plotdregion.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.colors import ListedColormap
+
+krfont =  {'family' : 'NanumGothic', 'weight' : 'bold', 'size' : 10}
+matplotlib.rc('font', **krfont)
+matplotlib.rcParams['axes.unicode_minus'] = False
+
+def plot_decision_region(X, y, classifier, test_idx=None, resolution=0.02, title='') :
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('r', 'b', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+    # 그리기
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+
+    Z = classifier.predict(np.array([xx.ravel(), yy.ravel()]).T)
+    Z = Z.reshape(xx.shape)
+
+    plt.contour(xx, yy, Z, alpha=0.5, cmap=cmap)
+    plt.xlim(xx.min(), xx.max())
+    plt.ylim(yy.min(), yy.max())
+
+    X_test, y_test = X[test_idx, :], y[test_idx]
+    for idx, cl in enumerate(np.unique(y)) :
+        plt.scatter(x=X[y==cl, 0], y=X[y==cl, 1], c=cmap(idx), marker=markers[idx], label=cl)
+
+
+
+    if test_idx :
+        X_test, y_test = X[test_idx, :], y[test_idx]
+        plt.scatter(X_test[:, 0], X_test[:, 1], c='', linewidths=1, edgecolors=cmap(0), marker='o', s=80, label='test data set')
+
+    plt.xlabel('표준화 된 꽃잎 길이')
+    plt.ylabel('표준화 된 꽃잎 너비')
+    plt.legend(loc=2)
+    plt.title(title)
+    plt.show()
